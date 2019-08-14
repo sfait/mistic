716acfbc783dbecf549cc6d0a5007a2bdd807326
Sylwia Fait <sylwia.fait@gmail.com>
2019-08-15 01:38:34 +0200

    Add gallery

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
+function showPhoto() {
+    const photos = document.querySelectorAll(".photo");
+
+    for(let i = 0; i < photos.length; i++) {
+        photos[i].addEventListener("click", function() {
+            this.querySelector("img").classList.toggle("photo-big");
+        })
+    }
+}
+
 const init = () => {
-    console.log("Hello World!");
+    showPhoto();
 };
 
 document.addEventListener("DOMContentLoaded", init);
